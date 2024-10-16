3109316c93488b5c1040cb670d162461e7754371
Jeroen ter Heerdt <jeroen_ter_heerdt@hotmail.com>
2024-10-16 11:40:19 -0700

    Add Slovenian to supported languages

--- custom_components/daily/const.py
+++ custom_components/daily/const.py
@@ -16,7 +16,7 @@ PLATFORMS = [SENSOR]
 
 # Localization
 LANGUAGE_FILES_DIR = "translations"
-SUPPORTED_LANGUAGES = ["da", "el", "en", "es", "fr", "nb", "nl", "sk"]
+SUPPORTED_LANGUAGES = ["da", "el", "en", "es", "fr", "nb", "nl", "sk", "sl"]
 
 # Config
 CONF_INPUT_SENSOR = "sensor"
